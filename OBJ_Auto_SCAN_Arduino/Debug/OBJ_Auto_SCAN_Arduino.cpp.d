78cbfefef20b76f44f4eec22fdc25e0fc6b44d0e
WeiCheng <wsx741405@gmail>
2018-02-14 03:49:42 +0800

    version 1.3.3 1. use visual studio arduino project to dev

---
+++ OBJ_Auto_SCAN_Arduino/Debug/OBJ_Auto_SCAN_Arduino.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\qaz74\AppData\Local\Temp\VMBuilds\OBJ_Auto_SCAN_Arduino\uno\Debug\OBJ_Auto_SCAN_Arduino.cpp.o: \
+ C:\Users\qaz74\AppData\Local\Temp\VMBuilds\OBJ_Auto_SCAN_Arduino\uno\Debug\OBJ_Auto_SCAN_Arduino.cpp \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Software\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ D:\Software\VS2015\Common7\IDE\Extensions\biwahlsd.4uq\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ D:\Software\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h
--- OBJ_Auto_SCAN_Arduino/OBJ_Auto_SCAN_Arduino.ino
+++ OBJ_Auto_SCAN_Arduino/OBJ_Auto_SCAN_Arduino.ino
@@ -1,13 +1,19 @@
+﻿/*
+ Name:		OBJ_Auto_SCAN_Arduino.ino
+ Created:	2018/2/14 上午 03:44:01
+ Author:	qaz74
+*/
+
 int data;
 
 void setup() {
-  Serial.begin(9600);
+	Serial.begin(9600);
 }
 
 void loop() {
-  if(Serial.available() > 0)
-  {
-    data = Serial.read();
-    Serial.write(data);
-  }
+	if (Serial.available() > 0)
+	{
+		data = Serial.read();
+		Serial.write(data);
+	}
 }
